00087c067c7a4316ad945e5b0b775ed9c6464836
임영규 <scv0128@gmail.com>
2020-01-20 00:12:31 +0900

    Implement TodoList and TodoListItem Components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import TodoTemplate from './components/TodoTemplate';
 import TodoInsert from './components/TodoInsert';
+import TodoList from './components/TodoList';
 
 const App = () => {
   return (
     <TodoTemplate>
       <TodoInsert />
+      <TodoList />
     </TodoTemplate>
   );
 };
