90fd1368952a512f13c23f8c90543179abe55bda
임영규 <scv0128@gmail.com>
2020-01-21 14:24:33 +0900

    Implement Toggling TodoListItem

--- src/components/TodoListItem.js
+++ src/components/TodoListItem.js
@@ -7,11 +7,11 @@ import {
 import cn from 'classnames';
 import './TodoListItem.scss';
 
-const TodoListItem = ({ todo, onRemove }) => {
+const TodoListItem = ({ todo, onRemove, onToggle }) => {
     const { id, text, checked } = todo;
     return (
         <div className='TodoListItem'>
-            <div className={cn('checkbox', { checked })}>
+            <div className={cn('checkbox', { checked })} onClick={() => onToggle(id)}>
                 { checked ? <MdCheckBox /> : <MdCheckBoxOutlineBlank />}
                 <div className='text'>{text}</div>
             </div>
