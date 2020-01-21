b44ea29a9ea6ec6517ea4f0bbc149087a8777eaa
임영규 <scv0128@gmail.com>
2020-01-21 14:31:39 +0900

    Updates

---
+++ precache-manifest.1f92af0d608b40d3c460dfaae8b53aaf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37c0b4bafdf7b53fb4c60fa0a203c9b3",
+    "url": "/todo_app/index.html"
+  },
+  {
+    "revision": "d0bdbdfdc4a3b0e4934c",
+    "url": "/todo_app/static/css/main.5ce63a43.chunk.css"
+  },
+  {
+    "revision": "3e648b15b787987276eb",
+    "url": "/todo_app/static/js/2.73136402.chunk.js"
+  },
+  {
+    "revision": "8a14dcea7ad8b593c1e83c11f7ba1a89",
+    "url": "/todo_app/static/js/2.73136402.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d0bdbdfdc4a3b0e4934c",
+    "url": "/todo_app/static/js/main.efcac816.chunk.js"
+  },
+  {
+    "revision": "9e9a4b01a7b8c64e8e2f",
+    "url": "/todo_app/static/js/runtime-main.086e6162.js"
+  }
+]);
